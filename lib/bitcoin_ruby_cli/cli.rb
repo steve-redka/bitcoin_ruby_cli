ed38cb614261bffa55ee5b60b673dae436673cbc
Steve Redka <cogitofallacy@gmail.com>
2025-07-07 11:26:17 +0300

    qol: CLI improvements, output readability

--- lib/bitcoin_ruby_cli/cli.rb
+++ lib/bitcoin_ruby_cli/cli.rb
@@ -1,31 +1,60 @@
 # frozen_string_literal: true
 
-require_relative "version"
-require_relative "wallet"
+require_relative 'version'
+require_relative 'wallet'
 require 'thor'
 
 module BitcoinRubyCli
   class CLI < Thor
-    desc "version", "Display the version of BitcoinRubyCli"
+    desc 'inspect', 'Inspect the Bitcoin wallet'
+    def inspect
+      if File.exist?('wallet.key')
+        wallet = Wallet.new
+        puts """
+          Wallet address: #{wallet.address}
+          Public key: #{wallet.pub_key}
+          Private key saved to 'wallet.key'.
+          Balance: #{wallet.balance / 1_000_000_000.0} BTC
+        """
+      else
+        puts "No wallet found. Please create a new wallet using the 'create' command."
+      end
+    end
+
+    desc 'version', 'Display the version of BitcoinRubyCli'
     def version
       puts "BitcoinRubyCli version #{BitcoinRubyCli::VERSION}"
     end
-    desc "create", "Create a new Bitcoin wallet"
+
+    desc 'create', 'Create a new Bitcoin wallet'
     def create
-      Wallet.new
+      if File.exist?('wallet.key')
+        puts 'Wallet already exists. Skipping creation.'
+      else
+        wallet = Wallet.new
+        puts """
+          New wallet created with address: #{wallet.address}, 
+          public key: #{wallet.pub_key}.
+          Private key saved to 'wallet.key'.
+        """
+      end
     end
-    desc "balance", "Fetch the balance of the Bitcoin wallet, in satoshis"
+
+    desc 'balance', 'Fetch the balance of the Bitcoin wallet, in satoshis'
     def balance
       p Wallet.new.balance
     end
-    desc "send RECEPIENT AMOUNT", "Send Bitcoin to a specified address"
+
+    desc 'send RECEPIENT AMOUNT', 'Send Bitcoin to a specified address'
     def send(recepient, amount)
-      if amount.include?('.')
-        amount = (amount.to_f * 100_000_000).to_i # Convert to satoshis
-      else
-        amount = amount.to_i
-      end
+      amount = if amount.include?('.')
+                 (amount.to_f * 100_000_000).to_i # Convert to satoshis
+               else
+                 amount.to_i
+               end
       Wallet.new.send_to(recepient, amount, 300)
     end
+
+    default_task :inspect
   end
 end
